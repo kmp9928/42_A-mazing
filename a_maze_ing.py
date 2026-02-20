9c4ad785b4ae5520c2098d50584b6743619f50be
Kimberly M <kimendon@student.codam.nl>
2026-02-20 12:41:23 +0100

    feat: add functions for main

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,29 +1,90 @@
 #!/usr/bin/env python3
 
-from mazegen import MazeGenerator, Config
+from mazegen import MazeGenerator, Maze, Config, ConfigParser, ConfigFileError, MazeGeneratorError
 from sukerl_print_ascii import AsciiPrinter
 from sukerl_create_output_txt import OutputGenerator
-# from gpt_maze_visualizer import print_maze
+from gpt_maze_visualizer import print_maze
+import sys
+
+
+def create_maze(config: Config, maze_generator: MazeGenerator) -> Maze:
+    maze = maze_generator.create_maze()
+    output = OutputGenerator()
+    output.create_output_txt(maze, config)
+    return maze
+
+
+def print_maze_path(maze: Maze, show_path: bool) -> None:
+    printer = AsciiPrinter()
+    printer.print_maze(maze)
+    if not show_path:
+        printer.toggle_path()
+
 
 if __name__ == "__main__":
-    print("=== Generate maze ===")
-    config = Config(
-        width=12,
-        height=12,
-        entry=(0, 0),
-        exit=(11, 11),
-        output_file="example.txt",
-        perfect=True
-    )
+    #create config
+    # create maze_gen
+    #crete maze
+    #ask on screen and action depedning on it
+    
+    # config = Config(
+    #     width=50,
+    #     height=40,
+    #     entry=(0, 0),
+    #     exit=(49, 39),
+    #     output_file="output_maze.txt",
+    #     perfect=False
+    # )
+    args = len(sys.argv)
+    if args == 1:
+        print("Configuration file missing. Please run again including it.")
+    else:
+        try:
+            config = ConfigParser().load(sys.argv[1])
+            maze_generator = MazeGenerator(config)
+        except (ConfigFileError, MazeGeneratorError) as e:
+            print(f"Error: {e}")
 
-    printer = AsciiPrinter()
-    printer.toggle_path()
-    output = OutputGenerator()
-    maze_generator = MazeGenerator(config)
+        maze = create_maze(config, maze_generator)
+        print_maze_path(maze, False)
+        choices: dict[int, str] = {
+            1: "Re-generate a new maze",
+            2: "Show/Hide path from entry to exit",
+            3: "Rotate maze colors",
+            4: "Quit"
+        }
 
-    maze = maze_generator.create()
-    printer.print_maze(maze)
-    output.create_output_txt(maze, config)
+        print("=== A-Maze-ing ===")
+        while True:
+            for num, info in choices.items():
+                print(f"{num}. {info}")
+            try:
+                choice = int(input("Choice? (1-4): "))
+            except ValueError:
+                print("\nPlease enter a number between 1 and 4.\n")
+                continue
+
+            if choice == 1:
+                maze = create_maze(config, maze_generator)
+                print_maze_path(maze, False)
+            elif choice == 2:
+                print_maze_path(maze, True)
+            elif choice == 3:
+                break
+            elif choice == 4:
+                break
+            else:
+                print("\nPlease enter a number between 1 and 4.\n")
+                continue
+
+
+        
+
+    # maze_generator = MazeGenerator(config)
+
+    # maze = maze_generator.create_maze()
+    # printer.print_maze(maze)
+    # output.create_output_txt(maze, config)
 
     # when run for the second time seed is not reset so new maze is created
     # maze = maze_generator.create()
@@ -33,4 +94,4 @@ if __name__ == "__main__":
     # for row in maze.grid:
     #     print(row)
     # print("FINAL")
-    # print_maze(maze_e, config.height, config.width)
+    # print_maze(maze, config.height, config.width)
